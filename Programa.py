7ac78b7ad3e1f55f263ba7250d8ef76c619c1cad
MiniMestri <fonsild97@hotmail.com>
2023-12-02 19:22:48 +0100

    Desarrollo del tablero y xml
    He desarrollado el código para que la ventana raíz se ejecuto con unas características por defecto.
    Además he hecho un primera prueba del xml

--- Programa.py
+++ Programa.py
@@ -1,19 +1,19 @@
 from bs4 import BeautifulSoup
 import tkinter as tk
+from tkinter import ttk
 
 raiz = tk.Tk()
 
-archivo = open("instruccion.xml","r")
-contenido = archivo.read()
-xml = BeautifulSoup(contenido,"xml")
-for campo in xml.find_all("campo"):
-    tipo = campo.get("tipo")
-    if tipo == "entrada":
-        tk.Entry(raiz).pack(padx=20,pady=20)
-    elif tipo== "etiqueta":
-        tk.Label(raiz,text="hola").pack(padx=20,pady=20)
-    elif tipo == "boton":
-        tkButton(raiz,text="hola").pack(padx=20,pady=20)
+raiz.geometry(f"{500}x{300}")
 
-tk.mainloop()
+ventana=tk.PanedWindow(raiz,orient=tk.HORIZONTAL)
+marco1=tk.Frame(ventana,background="yellow",width=250)
+marco2=tk.Frame(ventana,background="orange",width=250)
+
+ventana.add(marco1)
+ventana.add(marco2)
+
+ventana.pack(fill=tk.BOTH,expand=True)
+
+raiz.mainloop()
  
