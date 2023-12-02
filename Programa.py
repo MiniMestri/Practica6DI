1a07c4ea4ca6f009812c7da77a44d031f1665310
MiniMestri <fonsild97@hotmail.com>
2023-12-02 21:20:34 +0100

    Desarrollo de juego
    He desarrollado que el código funcional para que el juego de el resultado

--- Programa.py
+++ Programa.py
@@ -2,6 +2,22 @@ from bs4 import BeautifulSoup
 import tkinter as tk
 from tkinter import ttk
 
+def calculo(valor1,valor2):
+    valor1=int(spin1.get())
+    valor2=int(spin2.get())
+    resultado=valor1+valor2
+
+    if(resultado%2==0):
+        return "PARES"
+    else:
+        return "NONES"
+def jugar():
+    resultado1=spin1.get()
+    resultado2=spin2.get()
+
+    ttk.Label(marco1,text="EL GANADOR HA SIDO ",background="yellow").grid(row=3,column=0,padx=75,pady=20)
+    ttk.Label(marco2,text=calculo(resultado1,resultado2),background="orange").grid(row=3,column=0,padx=75,pady=20)
+    
 raiz = tk.Tk()
 
 raiz.geometry(f"{670}x{300}")
@@ -38,15 +54,13 @@ for texto in xml.find_all("texto"):
 for eleccion in xml.find_all("eleccion"):
     tipo=eleccion.get("tipo")
     if(tipo=="entry"):
-        ttk.Spinbox(marco1,from_=0,to=10).grid(row=2,column=0,padx=75,pady=20)
-        ttk.Spinbox(marco2,from_=0,to=10).grid(row=2,column=0,padx=75,pady=20)
+        spin1=ttk.Spinbox(marco1,from_=0,to=10)
+        spin1.grid(row=2,column=0,padx=75,pady=20)
+        spin2=ttk.Spinbox(marco2,from_=0,to=10)
+        spin2.grid(row=2,column=0,padx=75,pady=20)
         
-for eleccion in xml.find_all("resultado"):
-    tipo=eleccion.get("tipo")
-    if(tipo=="label"):
-        ttk.Label(marco1,text="El resultado ha sido ",background="yellow").grid(row=4,column=0,padx=75,pady=20)
-        ttk.Label(marco2,text="GANADOR ",background="orange").grid(row=4,column=0,padx=75,pady=20) 
 
+ttk.Button(ventana,text="JUGAR",command=lambda: jugar()).grid(row=4,column=0,padx=294,pady=250)
 
 raiz.mainloop()
  
